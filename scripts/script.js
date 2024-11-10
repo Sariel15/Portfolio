ade3f8b89b50872e6c42b4222fb925a3c54fa8b7
Shiro <cmarunrjay99@gmail.com>
2024-11-11 01:51:51 +0800

    update files

--- scripts/script.js
+++ scripts/script.js
@@ -3,10 +3,14 @@ const navLinks = document.querySelector('.nav-links');
 
 function setupEventListeners() {
     menuIcon.onclick = toggleNav;
+    document.getElementById('github-button').onclick = openGithub;
     document.getElementById('github-icon').onclick = openGithub;
     document.getElementById('facebook-icon').onclick = openFacebook;
     document.getElementById('linkedin-icon').onclick = openLinkedIn;
-    document.getElementById('project1-gitrepo').onclick = openProjectRepo;
+    document.getElementById('project1-gitrepo').onclick = openProjectRepo1;
+    document.getElementById('project2-gitrepo').onclick = openProjectRepo2;
+    document.getElementById('project3-gitrepo').onclick = openProjectRepo3;
+    document.getElementById('project4-gitrepo').onclick = openProjectRepo4;
     document.getElementById('download-cv').onclick = downloadCV;
     document.getElementById('submit-btn').onclick = validateForm;
 }
@@ -23,13 +27,24 @@ function openLinkedIn() {
     window.open('https://www.linkedin.com/in/rhoneetolentino', '_blank');
 }
 
-
 function openGithub() {
     window.open('https://www.github.com/Sariel15', '_blank');
 }
 
-function openProjectRepo() {
+function openProjectRepo1() {
     window.open('https://github.com/Sariel15/BlockChain-Charity-Donation', '_blank');
+}
+
+function openProjectRepo2() {
+    window.open('https://github.com/Sariel15/OOPLANG-Case-Study', '_blank');
+}
+
+function openProjectRepo3() {
+    window.open('https://github.com/Sariel15/OOPLANG-Case-Study', '_blank');
+}
+
+function openProjectRepo4() {
+    window.open('https://github.com/Sariel15/AppDev-TechCheck', '_blank');
 }
 
 function downloadCV() {
